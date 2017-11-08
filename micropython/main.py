3f9ec091552460703e0ebea60b061416fd9fe3c5
DurandA <arnaud.durand@live.com>
2017-11-08 17:59:04 +0100

    Add MicroPython project

---
+++ micropython/main.py
@@ -0,0 +1,45 @@
+from network import LoRa
+import socket
+import time
+import binascii
+import struct
+
+# Initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+
+# create an OTAA authentication parameters
+app_eui = binascii.unhexlify('70 B3 D5 7E D0 00 80 C3'.replace(' ',''))
+app_key = binascii.unhexlify('44 2D D2 30 0E D0 E4 96 7B BC CA 25 70 7E 4C 1E'.replace(' ',''))
+
+# join a network using OTAA (Over the Air Activation)
+lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+
+# wait until the module has joined the network
+while not lora.has_joined():
+    time.sleep(2.5)
+    print('Not yet joined...')
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+
+counter = 0
+while True:
+    # make the socket blocking
+    # (waits for the data to be sent and for the 2 receive windows to expire)
+    s.setblocking(True)
+
+    # send some data
+    s.send(struct.pack('>i', counter)[1:])
+
+    # make the socket non-blocking
+    # (because if there's no data received it will block forever...)
+    s.setblocking(False)
+
+    # get any data received (if any...)
+    data = s.recv(64)
+    print(data)
+
+    counter +=1
